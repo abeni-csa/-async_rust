ecccbef9dae1f22bfad68f4bad7912e81a5b349d
abeni-csa <abenezerhaymanot@gmail.com>
2025-04-12 11:43:51 +0300

    add folder structure

---
+++ target/debug/deps/async_rust-0324e64ad7e54a2a.d
@@ -0,0 +1,9 @@
+C:\Users\J-BURAK\Abeni_Rust\RustWebDev3rd\async_rust\target\debug\deps\libasync_rust-0324e64ad7e54a2a.rmeta: src\main.rs Cargo.toml
+
+C:\Users\J-BURAK\Abeni_Rust\RustWebDev3rd\async_rust\target\debug\deps\async_rust-0324e64ad7e54a2a.d: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ target/debug/deps/async_rust-9413006cb26fbd91.d
@@ -0,0 +1,9 @@
+C:\Users\J-BURAK\Abeni_Rust\RustWebDev3rd\async_rust\target\debug\deps\libasync_rust-9413006cb26fbd91.rmeta: src\main.rs Cargo.toml
+
+C:\Users\J-BURAK\Abeni_Rust\RustWebDev3rd\async_rust\target\debug\deps\async_rust-9413006cb26fbd91.d: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
