2ffffdbea526628cbb0a0810fea145759b83b745
abeni-csa <abenezerhaymanot@gmail.com>
2025-04-12 16:12:14 +0300

    send our async task to the executor straight away and then wait on it later can see that our time elapsed has halved! This is because our tokio::spawn sends the task to the tokio worker thread to be executed

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,11 @@ async fn do_something(number: i8) -> i8 {
 #[tokio::main(worker_threads = 1)]
 async fn main() {
     let now = time::Instant::now();
-    let future_one = do_something(1);
+    let future_one = tokio::spawn(do_something(1));
 
     let two_seconds = time::Duration::new(2, 0);
     thread::sleep(two_seconds);
-    let outcome = future_one.await;
+    let outcome = future_one.await.unwrap();
 
     println!("time elaspeld {:?}", now.elapsed());
     println!("Here is the outcome {}", outcome);
