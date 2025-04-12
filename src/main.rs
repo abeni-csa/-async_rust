f268440fe28304bcbe29326c2a2b433229aef2e6
abeni-csa <abenezerhaymanot@gmail.com>
2025-04-12 13:28:42 +0300

    classic example is to build a basic function that merely sleeps, blocking time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{thread, time};
+
+fn do_something(number: i8) -> i8 {
+    // This Funcition takes a number to exeute as UID
+    println!("number {} is runing", number);
+    let two_seconds = time::Duration::new(2, 0);
+    thread::sleep(two_seconds);
+    2
+}
+
 fn main() {
-    println!("Hello, world!");
+    let now = time::Instant::now();
+    let one: i8 = do_something(1);
+    let two: i8 = do_something(2);
+    let three: i8 = do_something(3);
+    println!("time elaspeld {:?}", now.elapsed());
+    println!("Result {}", one + two + three)
 }
