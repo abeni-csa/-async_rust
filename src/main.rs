6238665814aef32d4a3a8cc905bd08e47508c42b
abeni-csa <abenezerhaymanot@gmail.com>
2025-04-14 14:03:39 +0300

    Not working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use std::convert::Infallible;
 use std::env;
 use std::error::Error;
+use std::net::SocketAddr;
 use tokio::net::TcpListener;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+use hyper::rt::Executor;
+use hyper::server::conn::http2;
+use std::future::Future;
+
+async fn handle(body: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    println!("Recived a request");
+    println!("Headers: {:#?}", body.headers());
+    println!("Body: {:#?}", body.into_body());
+    Ok(Response::new(Full::new(Bytes::from("Hello World"))))
+}
+
+async fn false_main() -> Result<(), Box<dyn Error>> {
     let addr = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".to_string());
@@ -19,5 +37,34 @@ async fn main() -> Result<(), Box<dyn Error>> {
             // process the TCP request
             println!("Accepted connection from: {}", socket.peer_addr().unwrap());
         });
+    }
+}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 300));
+    let listiner = TcpListener::bind(&addr).await;
+    loop {
+        let (stream, _) = listiner.expect("END").accept().await?;
+        let io = TokioIo::new(stream);
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new(TokioExecutor)
+                .serve_connection(io, service_fn(handle))
+                .await
+            {
+                println!("Error Serving COnnection {:#?}", err);
+            }
+        });
+    }
+}
+#[derive(Clone)]
+struct TokioExecutor;
+
+impl<F> Executor<F> for TokioExecutor
+where
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    fn execute(&self, future: F) {
+        tokio::spawn(future);
     }
 }
